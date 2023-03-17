6c7b12a31f9528a2b8f1dc68a5c7054f1f7f176c
stevigno <ndjouonkep@gmail.com>
2023-03-17 12:56:06 -0700

    print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,26 +3,21 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-int i, j;
+int d;
 
-for (i = 0; i < 10; i++)
+for (d = '0'; d <= '9'; d++)
 {
-for (j = i; j < 10; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 9 || j != 9)
+putchar(d);
+if (d != '9')
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
 putchar('\n');
-
 return (0);
 }
