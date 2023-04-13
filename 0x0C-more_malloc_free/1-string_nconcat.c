cf4d7ae229c24804d4b1e521ccaad0977a177864
stevigno <ndjouonkep@gmail.com>
2023-04-13 14:20:12 -0700

    fix malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *string_nconcat 
+ * *string_nconcat
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
