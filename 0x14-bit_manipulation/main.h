3e32c8d67e4fc0c5de0a88ebc77b9a3eda1bda04
stevigno <ndjouonkep@gmail.com>
2023-07-06 08:33:51 -0700

    print_binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 
 #endif
