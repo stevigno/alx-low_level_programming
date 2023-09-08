21345ba283ebeadd7c22f6a4ff683a38320f30b4
stevigno <ndjouonkep@gmail.com>
2023-09-08 12:40:16 -0700

    fix1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -80,4 +80,5 @@ shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 
+
 #endif /* _HASH_TABLES_H_ */
